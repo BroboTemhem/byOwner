9f2ea11d9d307140bfb4cccaa505ccd093712377
Mehmet <brobotemhem@gmail.com>
2022-11-29 02:53:31 +0300

    OPENAPI added.

--- src/main/java/com/sarisite/byOwner/ByOwnerApplication.java
+++ src/main/java/com/sarisite/byOwner/ByOwnerApplication.java
@@ -1,7 +1,10 @@
 package com.sarisite.byOwner;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ByOwnerApplication {
@@ -10,4 +13,9 @@ public class ByOwnerApplication {
 		SpringApplication.run(ByOwnerApplication.class, args);
 	}
 
+	@Bean
+	public OpenAPI openAPI(){
+		return new OpenAPI()
+				.info(new Info());
+	}
 }
